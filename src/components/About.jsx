1c7885f232712e0c49122393728b8f4143cbef08
dinindu99 <peiristd-im18057@stu.kln.ac.lk>
2023-06-02 15:42:06 +0530

    fixed: overview section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import {Tilt} from 'react-tilt';
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { services } from '../constants';
 import {fadeIn, textVariant} from '../utils/motion';
+import { SectionWrapper } from '../hoc';
 
 const ServiceCard = ({index, title, icon}) => {
   return (
@@ -63,4 +63,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about");
